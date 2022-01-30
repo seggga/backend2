091a855b045407c6d646738454d8a0db61de2701
Serg Kotovsky <rokulab@mail.ru>
2022-01-30 22:51:47 +0300

    add postgres

--- internal/repo/postgres/init_db.sql
+++ internal/repo/postgres/init_db.sql
@@ -1,3 +1,63 @@
+/*
+ 
+инициализация БД
+
+*/
+
+-- =============================
+-- under user postgres:
+
+CREATE USER appuser WITH PASSWORD 'appp@$$w0rd';
+
+CREATE DATABASE app_db
+    TEMPLATE = 'template0'
+    ENCODING = 'utf-8'
+    LC_COLLATE = 'C.UTF-8'
+    LC_CTYPE = 'C.UTF-8';
+
+REVOKE ALL ON DATABASE app_db FROM PUBLIC;
+ALTER DATABASE app_db OWNER TO appuser;
+
+-- =============================
+-- connect to database:
+
+\c app_db
+
+-- =============================
+-- under user appuser:
+
+SET ROLE appuser;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    uuid CHAR(36) UNIQUE NOT NULL,
+    user_name VARCHAR(100) NOT NULL,
+    email VARCHAR(254) UNIQUE NOT NULL,
+    
+    PRIMARY KEY(uuid)
+);
+
+DROP TABLE IF EXISTS groups;
+CREATE TABLE groups (
+    uuid CHAR(36) UNIQUE NOT NULL,
+    group_name VARCHAR(50) UNIQUE NOT NULL,
+    group_type VARCHAR(50) NOT NULL,
+    descr VARCHAR(254) NOT NULL,
+    
+    PRIMARY KEY(uuid)
+);
+
+
+DROP TABLE IF EXISTS membership;
+CREATE TABLE membership (
+    group_uuid CHAR(36) REFERENCES groups (uuid),
+    user_uuid  CHAR(36) REFERENCES users (uuid),
+
+    constraint membership_fk_group_uuid FOREIGN KEY (group_uuid) references groups (uuid) on delete restrict,
+    constraint membership_fk_user_uuid FOREIGN KEY (user_uuid) references users (uuid) on delete restrict,
+    constraint membership_uniq_gid_uid_pair UNIQUE (group_uuid, user_uuid)
+);
+
 
 
 INSERT INTO users (uuid, user_name, email) VALUES
@@ -14,13 +74,13 @@ INSERT INTO users (uuid, user_name, email) VALUES
 
 
 INSERT INTO groups (uuid, group_name, group_type, descr) VALUES
-('c3b5be32-8510-49de-ba1a-7dc5102a95ab','Et dolorem accusantium dignissimos eaque qui ipsam.','Ea rem quis aut veritatis et sed et. Laboriosam asperiores a omnis dolores fuga. Provident vitae qui reprehenderit quo voluptas. Qui dignissimos quasi qui at quae dolor.'),
-('855ce14d-fb0b-47e0-9617-c8fa1ecafa8b','Tempora ab totam sed in quia laborum quia.','Culpa aut commodi vero modi commodi aliquid. Enim dolores qui sapiente beatae doloremque. Sunt omnis et fuga nihil quia ut excepturi. Dolores consequatur quis rerum voluptas. Fugiat ratione et quae dolore dolore.'),
-('10b29442-945d-433d-be56-2a9dc4094472','Delectus et quae officiis officia soluta.','Tempore laborum deleniti et officia ab et omnis. Possimus perferendis maxime itaque in. Vel hic suscipit temporibus et accusamus odit.'),
-('4b5dd85f-c53b-466f-b9a4-3eed66325442','Nobis nesciunt ea debitis.','Odit hic nostrum neque porro eum maiores harum. Harum fuga id tempore eum reprehenderit fugit error. Facilis fugit cupiditate at et dolorum quo ea.');
+('c3b5be32-8510-49de-ba1a-7dc5102a95ab','Dolorem accusantium','project','Laboriosam asperiores a omnis dolores fuga. Provident vitae qui reprehenderit quo voluptas.'),
+('855ce14d-fb0b-47e0-9617-c8fa1ecafa8b','Tempora ab totam','community','Culpa aut commodi vero modi commodi aliquid. Enim dolores qui sapiente beatae doloremque.'),
+('10b29442-945d-433d-be56-2a9dc4094472','Delectus et quae','organization','Tempore laborum deleniti et officia ab et omnis. Possimus perferendis maxime itaque in.'),
+('4b5dd85f-c53b-466f-b9a4-3eed66325442','Nobis nesciunt','project','Odit hic nostrum neque porro eum maiores harum. Harum fuga id tempore eum reprehenderit fugit error.');
 
 
-INSERT INTO membership (group_uuid, user_uuid) VALUES(
+INSERT INTO membership (group_uuid, user_uuid) VALUES
 ('c3b5be32-8510-49de-ba1a-7dc5102a95ab','fd0250b7-fed0-426d-991b-45bc9e927b14'),
 ('c3b5be32-8510-49de-ba1a-7dc5102a95ab','e707a24d-79c1-4d56-8d01-f79d30054a09'),
 ('c3b5be32-8510-49de-ba1a-7dc5102a95ab','206c5c37-41c5-49fa-b443-47e97839c0b8'),
