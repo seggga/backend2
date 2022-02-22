a653c7a7f9e6a48d2155970a5c94a04f92a7023b
Serg Kotovsky <rokulab@mail.ru>
2022-02-23 01:47:49 +0300

    add db metrics in red_vercors.go

--- internal/red/red_vectors.go
+++ internal/red/red_vectors.go
@@ -14,64 +14,64 @@ import "github.com/prometheus/client_golang/prometheus"
 var (
 	duration = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
-			Name:       "duration_seconds",
+			Name:       "backend2_duration_seconds",
 			Help:       "Summary of request duration in seconds",
 			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
 		},
-		[]string{"URI", "METHOD", "CODE"},
+		[]string{"URI", "METHOD"},
 	)
 	errorsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "errors_total",
+			Name: "backend2_errors_total",
 			Help: "Total number of errors",
 		},
 		[]string{"URI", "METHOD", "CODE"},
 	)
 	requestsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "request_total",
+			Name: "backend2_request_total",
 			Help: "Total number of requests",
 		},
 		[]string{"URI", "METHOD"},
 	)
 )
 
-// var (
-// 	// Duration ...
-// 	Duration = prometheus.NewSummaryVec(
-// 		prometheus.SummaryOpts{
-// 			Name:       "out_duration_seconds",
-// 			Help:       "Summary of ongoing request duration in seconds",
-// 			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
-// 		},
-// 		[]string{labelService, labelQuery, labelResult},
-// 	)
+var (
+	DurationDBFunc = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "backend2_duration_db_method_seconds",
+			Help:       "Summary of database method request duration in seconds",
+			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
+		},
+		[]string{"FUNC"},
+	)
 
-// 	// ErrorsTotal ...
-// 	ErrorsTotal = prometheus.NewCounterVec(
-// 		prometheus.CounterOpts{
-// 			Name: "out_errors_total",
-// 			Help: "Total number of ongoing errors",
-// 		},
-// 		[]string{labelService, labelQuery, labelResult},
-// 	)
+	DurationDBErrors = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "backend2_duration_query_db_method_seconds",
+			Help: "Summary of database Query method request duration in seconds",
+		},
+		[]string{"URI", "METHOD"},
+	)
 
-// 	// RequestsTotal ...
-// 	RequestsTotal = prometheus.NewCounterVec(
-// 		prometheus.CounterOpts{
-// 			Name: "out_request_total",
-// 			Help: "Total number of ongoing requests",
-// 		},
-// 		[]string{labelService, labelQuery},
-// 	)
-// )
+	DurationDBRequests = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "backend2_duration_query_row_db_method_seconds",
+			Help:       "Summary of database QueryRow method request duration in seconds",
+			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
+		},
+		[]string{"URI", "METHOD"},
+	)
+)
 
 func init() {
 	prometheus.MustRegister(duration)
 	prometheus.MustRegister(errorsTotal)
 	prometheus.MustRegister(requestsTotal)
 
-	// prometheus.MustRegister(Duration)
-	// prometheus.MustRegister(ErrorsTotal)
-	// prometheus.MustRegister(RequestsTotal)
+	prometheus.MustRegister(DurationDBFunc)
+	prometheus.MustRegister(DurationDBErrors)
+	prometheus.MustRegister(DurationDBRequests)
 }
+
+// todo: написать обертку для обращения к БД, втиснуть в нее метрики
