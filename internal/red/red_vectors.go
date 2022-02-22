13e92e537a769d7dc4d7930cbfff49a007e489bc
Serg Kotovsky <rokulab@mail.ru>
2022-02-22 12:51:34 +0300

    comment unused metrics in red_vectors.go

--- internal/red/red_vectors.go
+++ internal/red/red_vectors.go
@@ -2,14 +2,14 @@ package red
 
 import "github.com/prometheus/client_golang/prometheus"
 
-const (
-	labelHandler = "my_handler"
-	labelMethod  = "my_method"
-	labelStatus  = "my_status"
-	labelQuery   = "my_query"
-	labelResult  = "my_result"
-	labelService = "my_service"
-)
+// const (
+// 	labelHandler = "my_handler"
+// 	labelMethod  = "my_method"
+// 	labelStatus  = "my_status"
+// 	labelQuery   = "my_query"
+// 	labelResult  = "my_result"
+// 	labelService = "my_service"
+// )
 
 var (
 	duration = prometheus.NewSummaryVec(
@@ -18,60 +18,60 @@ var (
 			Help:       "Summary of request duration in seconds",
 			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
 		},
-		[]string{labelHandler, labelMethod, labelStatus},
+		[]string{"URI", "METHOD", "CODE"},
 	)
 	errorsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "errors_total",
 			Help: "Total number of errors",
 		},
-		[]string{labelHandler, labelMethod, labelStatus},
+		[]string{"URI", "METHOD", "CODE"},
 	)
 	requestsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "request_total",
 			Help: "Total number of requests",
 		},
-		[]string{labelHandler, labelMethod},
+		[]string{"URI", "METHOD"},
 	)
 )
 
-var (
-	// Duration ...
-	Duration = prometheus.NewSummaryVec(
-		prometheus.SummaryOpts{
-			Name:       "out_duration_seconds",
-			Help:       "Summary of ongoing request duration in seconds",
-			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
-		},
-		[]string{labelService, labelQuery, labelResult},
-	)
+// var (
+// 	// Duration ...
+// 	Duration = prometheus.NewSummaryVec(
+// 		prometheus.SummaryOpts{
+// 			Name:       "out_duration_seconds",
+// 			Help:       "Summary of ongoing request duration in seconds",
+// 			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
+// 		},
+// 		[]string{labelService, labelQuery, labelResult},
+// 	)
 
-	// ErrorsTotal ...
-	ErrorsTotal = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "out_errors_total",
-			Help: "Total number of ongoing errors",
-		},
-		[]string{labelService, labelQuery, labelResult},
-	)
+// 	// ErrorsTotal ...
+// 	ErrorsTotal = prometheus.NewCounterVec(
+// 		prometheus.CounterOpts{
+// 			Name: "out_errors_total",
+// 			Help: "Total number of ongoing errors",
+// 		},
+// 		[]string{labelService, labelQuery, labelResult},
+// 	)
 
-	// RequestsTotal ...
-	RequestsTotal = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "out_request_total",
-			Help: "Total number of ongoing requests",
-		},
-		[]string{labelService, labelQuery},
-	)
-)
+// 	// RequestsTotal ...
+// 	RequestsTotal = prometheus.NewCounterVec(
+// 		prometheus.CounterOpts{
+// 			Name: "out_request_total",
+// 			Help: "Total number of ongoing requests",
+// 		},
+// 		[]string{labelService, labelQuery},
+// 	)
+// )
 
 func init() {
 	prometheus.MustRegister(duration)
 	prometheus.MustRegister(errorsTotal)
 	prometheus.MustRegister(requestsTotal)
 
-	prometheus.MustRegister(Duration)
-	prometheus.MustRegister(ErrorsTotal)
-	prometheus.MustRegister(RequestsTotal)
+	// prometheus.MustRegister(Duration)
+	// prometheus.MustRegister(ErrorsTotal)
+	// prometheus.MustRegister(RequestsTotal)
 }
